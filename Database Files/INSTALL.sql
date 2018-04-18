f0c464773b735e4569bab4ed7e8f8523af9862be
Marvin <mj03505@georgiasouthern.edu>
2018-04-18 12:39:51 -0400

    Add "TransitionItem" Table

--- Database Files/INSTALL.sql
+++ Database Files/INSTALL.sql
@@ -1,10 +1,9 @@
-
 -- phpMyAdmin SQL Dump
 -- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 27, 2018 at 12:37 AM
+-- Generation Time: Apr 18, 2018 at 06:36 PM
 -- Server version: 10.1.30-MariaDB
 -- PHP Version: 7.2.1
 
@@ -19,111 +18,196 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
-CREATE SCHEMA IF NOT EXISTS `DistWebProject` DEFAULT CHARACTER SET latin1 ;
-USE `DistWebProject` ;
-
 --
--- Database: `dist.web_project`
+-- Database: `distwebproject`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `CreditCard`
+-- Table structure for table `creditcard`
 --
 
-CREATE TABLE IF NOT EXISTS `CreditCard` (
-	`CCID` int NOT NULL,
-	`UserID` int NOT NULL,
-	`Name` varchar(255) NOT NULL,
-	`CC_Number` varchar(20) NOT NULL,
-	`Exp_Month` int NOT NULL,
-	`Exp_Day` int NOT NULL,
-	PRIMARY KEY(`CCID`)
-    )
-ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `creditcard` (
+  `CCID` int(11) NOT NULL,
+  `UserID` int(11) NOT NULL,
+  `Name` varchar(255) NOT NULL,
+  `CC_Number` varchar(20) NOT NULL,
+  `Exp_Month` int(11) NOT NULL,
+  `Exp_Day` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `User`
+-- Table structure for table `creditcarduser`
 --
 
-CREATE TABLE IF NOT EXISTS `User` (
-	`UserID` int NOT NULL,
-	`Username` varchar(255) NOT NULL,
-	`Password` varchar(255) NOT NULL,
-	`EmailAddress` varchar(255) COMMENT 'User Email',
-	`Address` varchar(255) COMMENT 'User Address',
-	`PhoneNumber` varchar(20) COMMENT 'User Phone Number',
-    PRIMARY KEY(`UserID`)
-	) 
-ENGINE=InnoDB AUTO_INCREMENT = 9 DEFAULT CHARSET=latin1;
+CREATE TABLE `creditcarduser` (
+  `CCID` int(11) NOT NULL,
+  `UserID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `movies`
+-- Table structure for table `dvd`
 --
 
-CREATE TABLE `Movie` (
-	`MovieID` int NOT NULL,
-	`Movie_Title` varchar(1000) NOT NULL COMMENT '1000 char limit for absurdly long movie titles.',
-	`Genre` varchar(255) NOT NULL COMMENT 'Each movie may only have 1 genre for now.',
-	`Price` decimal(13,2) NOT NULL,
-	`Rating` varchar(255),
-	`Description` varchar(2500) COMMENT '2500 character limit for the movie description',
-    PRIMARY KEY(`MovieID`)
-	) 
-ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `dvd` (
+  `DVDID` int(11) NOT NULL,
+  `MovieID` int(11) NOT NULL,
+  `UserID` int(11) DEFAULT NULL COMMENT 'Is null for DVDs not out on rent.',
+  `Condition` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `CreditCardUser`
+-- Table structure for table `movie`
 --
 
-CREATE TABLE IF NOT EXISTS `CreditCardUser` (
-	`CCID` int NOT NULL,
-	`UserID` int NOT NULL,
-	PRIMARY KEY(`CCID`, `UserID`),
-    FOREIGN KEY (`CCID`) REFERENCES `CreditCard`(`CCID`),
-    FOREIGN KEY (`UserID`) REFERENCES `User`(`UserID`)
-    )
-ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `movie` (
+  `MovieID` int(11) NOT NULL,
+  `Movie_Title` varchar(1000) NOT NULL COMMENT '1000 char limit for absurdly long movie titles.',
+  `Genre` varchar(255) NOT NULL COMMENT 'Each movie may only have 1 genre for now.',
+  `Price` decimal(13,2) NOT NULL,
+  `Rating` varchar(255) DEFAULT NULL,
+  `Description` varchar(2500) DEFAULT NULL COMMENT '2500 character limit for the movie description'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `DVD`
+-- Table structure for table `transaction`
 --
 
-CREATE TABLE IF NOT EXISTS `DVD` (
-	`DVDID` int NOT NULL,
-	`MovieID` int NOT NULL,
-	`UserID` int COMMENT 'Is null for DVDs not out on rent.',
-	`Condition` varchar(255) NOT NULL,
-	PRIMARY KEY(`DVDID`, `MovieID`),
-    FOREIGN KEY (`MovieID`) REFERENCES `Movie`(`MovieID`),
-    FOREIGN KEY (`UserID`) REFERENCES `User`(`UserID`)
-	) 
-ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `transaction` (
+  `TransactionID` int(11) NOT NULL,
+  `UserID` int(11) NOT NULL,
+  `CCID` int(11) NOT NULL,
+  `Total_Charge` decimal(13,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Transaction`
+-- Table structure for table `transactionitem`
 --
 
-CREATE TABLE IF NOT EXISTS  `Transaction` (
-	`TransactionID` int NOT NULL,
-	`UserID` int NOT NULL,
-	`CCID` int NOT NULL,
-	`Total_Charge` decimal(13,2) NOT NULL,
-	PRIMARY KEY(`TransactionID`, `UserID`),
-	FOREIGN KEY (`UserID`) REFERENCES `User`(`UserID`),
-    FOREIGN KEY (`CCID`) REFERENCES `CreditCard`(`CCID`)
-	) 
-ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `transactionitem` (
+  `TransactionID` int(11) NOT NULL,
+  `UserID` int(11) NOT NULL,
+  `TransactionItemID` int(11) NOT NULL,
+  `DVDID` int(11) NOT NULL,
+  `MovieID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `UserID` int(11) NOT NULL,
+  `Username` varchar(255) NOT NULL,
+  `Password` varchar(255) NOT NULL,
+  `EmailAddress` varchar(255) DEFAULT NULL COMMENT 'User Email',
+  `Address` varchar(255) DEFAULT NULL COMMENT 'User Address',
+  `PhoneNumber` varchar(20) DEFAULT NULL COMMENT 'User Phone Number'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `creditcard`
+--
+ALTER TABLE `creditcard`
+  ADD PRIMARY KEY (`CCID`);
+
+--
+-- Indexes for table `creditcarduser`
+--
+ALTER TABLE `creditcarduser`
+  ADD PRIMARY KEY (`CCID`,`UserID`),
+  ADD KEY `UserID` (`UserID`);
+
+--
+-- Indexes for table `dvd`
+--
+ALTER TABLE `dvd`
+  ADD PRIMARY KEY (`DVDID`,`MovieID`),
+  ADD KEY `MovieID` (`MovieID`),
+  ADD KEY `UserID` (`UserID`);
+
+--
+-- Indexes for table `movie`
+--
+ALTER TABLE `movie`
+  ADD PRIMARY KEY (`MovieID`);
+
+--
+-- Indexes for table `transaction`
+--
+ALTER TABLE `transaction`
+  ADD PRIMARY KEY (`TransactionID`,`UserID`),
+  ADD KEY `UserID` (`UserID`),
+  ADD KEY `CCID` (`CCID`);
+
+--
+-- Indexes for table `transactionitem`
+--
+ALTER TABLE `transactionitem`
+  ADD PRIMARY KEY (`TransactionItemID`),
+  ADD UNIQUE KEY `TransactionID` (`TransactionID`),
+  ADD UNIQUE KEY `UserID` (`UserID`),
+  ADD UNIQUE KEY `DVDID` (`DVDID`),
+  ADD UNIQUE KEY `MovieID` (`MovieID`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`UserID`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `creditcarduser`
+--
+ALTER TABLE `creditcarduser`
+  ADD CONSTRAINT `creditcarduser_ibfk_1` FOREIGN KEY (`CCID`) REFERENCES `creditcard` (`CCID`),
+  ADD CONSTRAINT `creditcarduser_ibfk_2` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`);
+
+--
+-- Constraints for table `dvd`
+--
+ALTER TABLE `dvd`
+  ADD CONSTRAINT `dvd_ibfk_1` FOREIGN KEY (`MovieID`) REFERENCES `movie` (`MovieID`),
+  ADD CONSTRAINT `dvd_ibfk_2` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`);
+
+--
+-- Constraints for table `transaction`
+--
+ALTER TABLE `transaction`
+  ADD CONSTRAINT `transaction_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`),
+  ADD CONSTRAINT `transaction_ibfk_2` FOREIGN KEY (`CCID`) REFERENCES `creditcard` (`CCID`);
+
+--
+-- Constraints for table `transactionitem`
+--
+ALTER TABLE `transactionitem`
+  ADD CONSTRAINT `transactionitem_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `transactionitem_ibfk_2` FOREIGN KEY (`TransactionID`) REFERENCES `transaction` (`TransactionID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `transactionitem_ibfk_3` FOREIGN KEY (`DVDID`) REFERENCES `dvd` (`DVDID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `transactionitem_ibfk_4` FOREIGN KEY (`MovieID`) REFERENCES `movie` (`MovieID`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
